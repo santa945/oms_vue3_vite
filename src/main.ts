f742286dfa6d41713243efdbcf99ea05f8e24e78
曾胜达 <santa945@aliyun.com>
2021-12-05 17:51:42 +0800

    add: 配置环境变量

--- src/main.ts
+++ src/main.ts
@@ -10,5 +10,6 @@ app.use(store)
 app.mount('#app');
 
 console.log('router', router);
+console.log('环境变量', import.meta.env.VITE_MODE);
 console.log('store', store);
 console.log('实例', app);
--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,43 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-export default defineConfig({
-    server: {
-        open: true,
-        host: '127.0.0.1',
-        port: 2021,
-        proxy: {
-            '/api': {
-                target: '127.0.0.1:3434',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
-            },
-        }
-    },
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'src'),
-        }
-    },
-    plugins: [
-        vue(),
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()],
-        }),
-    ]
+
+export default (({ mode }) => {
+    console.log('环境变量', mode);
+    console.log('环境变量里的文件对象', loadEnv(mode, process.cwd()));
+
+    return defineConfig({
+        server: {
+            open: true,
+            host: '127.0.0.1',
+            port: 2021,
+            proxy: {
+                '/api': {
+                    target: '127.0.0.1:3434',
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, '')
+                },
+            }
+        },
+        build: {
+            sourcemap: loadEnv(mode, process.cwd()).VITE_MODE !== 'prod' // 生产环境不要源码文件
+        },
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, 'src'),
+            }
+        },
+        plugins: [
+            vue(),
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()],
+            }),
+        ]
+    })
 })
